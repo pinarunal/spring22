1035641f4990235b279c8efdad74c4e2c5999ddb
yasinaniltechpro <105437708+yasinaniltechpro@users.noreply.github.com>
2022-06-08 15:15:35 +0300

    day14

---
+++ src/main/java/practice03/Q02.java
@@ -0,0 +1,15 @@
+package practice03;
+
+public class Q02 {
+    /*
+    Print on the console numbers from 1 to 100.
+    But do not use any number in your code!
+    */
+    public static void main(String[] args) {
+
+        for(int i='a'/'a'; i<='d'; i++){
+            System.out.print(i+" ");
+        }
+
+    }
+}
